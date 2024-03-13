1febccbfcaae647b1115ac290d0f9579b25d5413
Mehmet Ali Soylu <mhmtsoylu1928@gmail.com>
2024-03-13 15:04:15 +0300

    Update main.py
    Change time labels to 00:00
    Change start & stop record time functions

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-
 import customtkinter as ctk
 import tkinter as tk
 from time import time
 from openpyxl import Workbook
-
 
 wb = None
 
@@ -45,7 +43,7 @@ class App(ctk.CTk):
 
         self.total_recorded_time_label = ctk.CTkLabel(
             self.frame_for_result, 
-            text="Total recorded time: 00:00:00", 
+            text="Total recorded time: 00:00", 
             justify="center"
         )
 
@@ -83,7 +81,7 @@ class App(ctk.CTk):
     def create_record_time_label(self):
         self.record_time_label = ctk.CTkLabel(
             self.frame_for_buttons,
-            text="Recorded Intervals: 00:00:00",
+            text="Recorded Intervals: 00:00",
             font=("Arial", 15),
             justify="center"
         )
@@ -92,7 +90,7 @@ class App(ctk.CTk):
     def create_record_label(self):
         self.record_time = ctk.CTkLabel(
             self.frame_for_buttons, 
-            text="Record Time: 00:00:00", 
+            text="Record Time: 00:00", 
             font=("Arial", 15),
             justify="center"
         )
@@ -117,7 +115,7 @@ class App(ctk.CTk):
     def stop(self):
         if self.timer_running:
             self.timer_running = False
-        self.timer_label.configure(text="Time: 00:00:00")
+        self.timer_label.configure(text="Time: 00:00")
         self.elapsed_time = 0
         self.start_time = None  # Updated this line
             
@@ -130,38 +128,44 @@ class App(ctk.CTk):
     def stop_record(self, event):
         if self.start_record_time:
             elapsed_time = time() - self.start_record_time
-            recorded_seconds = int(elapsed_time)
+            seconds, milliseconds = self.calculate_seconds_and_milliseconds(elapsed_time)
 
-            timer_elapsed = time() - self.start_time
-            timer_hours = int(timer_elapsed // 3600)
-            timer_minutes = int((timer_elapsed % 3600) // 60)
-            timer_seconds = int(timer_elapsed % 60)
-            milliseconds = int((elapsed_time % 1) * 1000)
-            seconds = int(elapsed_time)
-
-            self.record_time.configure(text=f"Record Time: {seconds:02d}s {milliseconds:03d}ms")
+            self.record_time.configure(text=f"Record Time: {seconds:02d}:{milliseconds:03d}")
             self.start_record_time = None  # Stop the recording here
 
-            #Save to excel
-            self.save_to_excel(self.record_number, recorded_seconds, timer_hours, timer_minutes, timer_seconds)
+            # Save the current time when the record button is pressed
+            timer_time_str = self.timer_label.cget("text")
+
+            # Save to excel
+            self.save_to_excel(self.record_number, seconds, milliseconds, timer_time_str)
+
+            # Append the recorded interval along with its start time to record times
+            self.record_times.append(((seconds, milliseconds), timer_time_str))
 
             # Get current text in the text widget and append the new recorded time
             self.record_text.get("1.0", tk.END).strip()
-            new_record = f"Recorded Intervals: {seconds:02d}:{milliseconds:03d}s"
+            new_record = f"Recorded Intervals: {seconds:02d}:{milliseconds:03d} -- {timer_time_str}\n"
             self.record_text.insert(tk.END, f"{new_record}\n")
 
             # Update the total recorded time
-            self.total_recorded_time += recorded_seconds
-            total_recorded_time_str = f"Total recorded time: {self.total_recorded_time//3600:02d}:{(self.total_recorded_time%3600)//60:02d}:{self.total_recorded_time%60:02d}"
+            self.total_recorded_time += seconds
+            self.total_recorded_time += milliseconds / 1000
+            total_seconds = int(self.total_recorded_time)
+            total_milliseconds = int((self.total_recorded_time - total_seconds) * 1000)  # Extract milliseconds
+            total_recorded_time_str = f"Total recorded time: {total_seconds:02d}:{total_milliseconds:03d}"
             self.total_recorded_time_label.configure(text=total_recorded_time_str)
-            self.record_times.append((recorded_seconds, (timer_hours,timer_minutes, timer_seconds)))
 
             self.record_text.delete("1.0", tk.END)
-            for i, (recorded_seconds, timer_value) in enumerate(self.record_times):
-                record_str = f"Record#{i+1} -- {recorded_seconds:02d}s -- (Match Time:{timer_value[0]:2d}:{timer_value[1]:02d}:{timer_value[2]:02d})\n"
+            for i, ((seconds, milliseconds), timer_time_str) in enumerate(self.record_times):
+                record_str = f"Record#{i + 1} -- {seconds:02d}:{milliseconds:03d} -- {timer_time_str}\n"
                 self.record_text.insert(tk.END, record_str)
-            
-            self.record_start_time = None            
+
+            self.record_start_time = None
+
+    def calculate_seconds_and_milliseconds(self, elapsed_time):
+        seconds = int(elapsed_time)
+        milliseconds = int((elapsed_time - seconds) * 1000)
+        return seconds, milliseconds
 
     def update_timer(self):
         if self.timer_running:
@@ -177,52 +181,34 @@ class App(ctk.CTk):
             if self.record_start_time:
                 elapsed_time = time() - self.record_start_time
                 seconds = int(elapsed_time)
-                milliseconds = int((elapsed_time % 1) * 1000)
+                milliseconds = int((elapsed_time - seconds) * 1000)  # Extract milliseconds
 
                 self.record_time_label.configure(text=f"Recorded Intervals: {seconds:02d}:{milliseconds:03d}")
-
-    def update_total_recorded_time_label(self):
-        milliseconds = int((self.total_recorded_time %1) * 1000)
-        seconds = int(self.total_recorded_time)
-        total_recorded_time_str = f"Total recorded time: {seconds:02d}s :{milliseconds:03d}ms"
-        self.total_recorded_time_label.configure(text=total_recorded_time_str)
-
-    def update_recorded_time_label(self):
-        if self.record_start_time:
-            elapsed_time = time() - self.record_start_time
-            seconds = int(elapsed_time)
-            milliseconds = int((elapsed_time % 1) * 1000)            
-        else:
-            seconds = 0
-            milliseconds = 0
-
-        self.record_time_label.configure(text=f"Recorded Intervals: {seconds:02d}:{milliseconds:03d}")
-        self.after(1, self.update_recorded_time_label) 
 
     def clear_records(self):
         self.record_text.delete("1.0", tk.END)
         self.record_times = []
         self.total_recorded_time = 0
-        self.update_total_recorded_time_label()
+        self.total_recorded_time_label.configure(text="Total recorded time: 00:00")
         self.record_number = 0
         self.start_record_time = None
-        self.update_recorded_time_label()
-        self.record_time.configure(text="Record Time: 00:00:00")
+        self.record_time.configure(text="Record Time: 00:000")
+        self.record_time_label.configure(text="Recorded Intervals: 00:000")
         
-    def save_to_excel(self, record_number, recorded_seconds, timer_hours, timer_minutes, timer_seconds):
+    def save_to_excel(self, record_number, seconds, milliseconds, timer_time_str):
         global wb
         try:
             if wb is None:
                 wb = Workbook()
                 ws = wb.active
-                ws.append(["Record Number","Record Time (ms)","Total Time (HH:MM:SS)"])
+                ws.append(["Record Number","Record Time (SS:MMM)","Start Time"])
             
             ws = wb.active
-            raw_data = [record_number, recorded_seconds, f"{timer_hours:02d}:{timer_minutes:02d}:{timer_seconds:02d}"]
+            raw_data = [record_number, f"{seconds:02d}:{milliseconds:03d}", timer_time_str]
             ws.append(raw_data)
             
             wb.save("match_data.xlsx")
-            print("Excel saved succesfully")
+            print("Excel saved successfully")
 
         except Exception as e:
             print(f"Error saving to excel {e}")
